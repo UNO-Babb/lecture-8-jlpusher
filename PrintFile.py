bae3cfd9ef3e049110444ba9f5d818992e3c66d1
jlpusher <jlpusher@unomaha.edu>
2025-05-10 20:44:41 +0000

    During lecture video

--- PrintFile.py
+++ PrintFile.py
@@ -1,8 +1,13 @@
 def main():
   myFile = open("qbdata.txt", 'r')
 
-  for line in myFile:
-    print (line)
+  for line in myFile: #my code matches yours from the lecture video but for some reason info is showing as only one spot instead of each item being its own.
+    info = line.split(",") 
+    td = info[12]
+    name = info[0]
+    rating = info[1]
+    print (name, "had a rating of", rating, "and threw", td, "touchdowns.") 
+    
 
   myFile.close()
 
