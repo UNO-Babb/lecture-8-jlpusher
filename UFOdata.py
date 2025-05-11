aca889459d9e289de7a94fcf19f93f1ccc51c132
jlpusher <jlpusher@unomaha.edu>
2025-05-11 03:59:37 +0000

    ufo data update

---
+++ UFOdata.py
@@ -0,0 +1,17 @@
+def main():
+    myFile = open("ufo_sightings.csv", 'r')
+
+#can you print all the data?
+#can you isolate one state?
+#can you find the sum total of cases by place?
+#Are there any data anomalies you would need to consider?
+
+    for line in myFile:
+        info = line.split(",")
+        state = info[1]
+        city = info[0]
+        print(state)
+        #info = line.split(",")
+
+if __name__ == '__main__':
+    main()
